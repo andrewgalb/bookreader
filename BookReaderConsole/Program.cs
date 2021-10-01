41b040c2e390ef83c3ca5d1449ce92f9a09d6299
Someonexddd <50412783+Someonexddd@users.noreply.github.com>
2021-10-01 08:32:58 +0200

    Working CRUD

--- BookReaderConsole/Program.cs
+++ BookReaderConsole/Program.cs
@@ -27,20 +27,26 @@ namespace BookReaderConsole
             */
 
             //View all books and one specific book
+            
             Index();
             Details("1-86092-022-5");
+            
             
             //Create  a book, then view all books to see if book has been created...
             Create("The Art of Motorcycle Mainentance", "Robert Maynard Pirsig", "Self-Destruction", "0-688-00230-7");
             Index();
-
+            
+            
             //Update a book, then check to see if that specific book has been updated
             Update("The Art of Motorcycle Mainentance", "Robert Maynard Pirsig", "Self-Help", "0-688-00230-7");
             Details("0-688-00230-7");
+            
 
+            
             //Delete a book, then view all books to make sure "The Open Boat" has been deleted
             Delete("1-86092-025-x");
             Index();
+            
         }
 
 
@@ -49,7 +55,25 @@ namespace BookReaderConsole
         /// </summary>
         static void Index()
         {
+            List<Book> books = new List<Book>();
 
+            using (StreamReader sr = new StreamReader("Books.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string title = sr.ReadLine();
+                    string author = sr.ReadLine();
+                    string genre = sr.ReadLine();
+                    string isbn = sr.ReadLine();
+
+                    //  Book book1 = new Book(title, author, genre, isbn);
+                    books.Add(new Book(title, author, genre, isbn));
+                }
+            }
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine("Title: {0, -20}\tAuthor: {1, -20}\tGenre: {2, -20}\tISBN: {3, -20}\t", books[i].Title, books[i].Author, books[i].Genre, books[i].ISBN);
+            }
         }
 
         /// <summary>
@@ -58,6 +82,30 @@ namespace BookReaderConsole
         /// <param name="ISBN"></param>
         static void Details(string ISBN)
         {
+            Console.WriteLine("Details");
+            List<Book> books = new List<Book>();
+
+            using (StreamReader sr = new StreamReader("Books.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string title = sr.ReadLine();
+                    string author = sr.ReadLine();
+                    string genre = sr.ReadLine();
+                    string isbn = sr.ReadLine();
+
+                    //  Book book1 = new Book(title, author, genre, isbn);
+                    books.Add(new Book(title, author, genre, isbn));
+                }
+                for (int i = 0; i < books.Count; i++)
+                {
+                    if (books[i].ISBN == ISBN)
+                    {
+                        Console.WriteLine("Title: {0}\tAuthor: {1}\tGenre: {2}\tISBN: {3}\t", books[i].Title, books[i].Author, books[i].Genre, books[i].ISBN);
+                        break;
+                    }
+                }
+            }
 
         }
 
@@ -70,7 +118,32 @@ namespace BookReaderConsole
         /// <param name="ISBN"></param>
         static void Create(string Title, string Author, string Genre, string ISBN)
         {
+            List<Book> books = new List<Book>();
 
+            using (StreamReader sr = new StreamReader("Books.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string title = sr.ReadLine();
+                    string author = sr.ReadLine();
+                    string genre = sr.ReadLine();
+                    string isbn = sr.ReadLine();
+
+                    //  Book book1 = new Book(title, author, genre, isbn);
+                    books.Add(new Book(title, author, genre, isbn));
+                }
+            }
+
+            books.Add(new Book(Title, Author, Genre, ISBN));
+
+
+            using (StreamWriter sw = new StreamWriter("Books.txt"))
+            {
+                for (int i = 0; i < books.Count; i++)
+                {
+                    sw.WriteLine("{0}\n{1}\n{2}\n{3}", books[i].Title, books[i].Author, books[i].Genre, books[i].ISBN);
+                }
+            }
         }
 
 
@@ -83,8 +156,40 @@ namespace BookReaderConsole
         /// <param name="ISBN"></param>
         static void Update(string Title, string Author, string Genre, string ISBN)
         {
+            List<Book> books = new List<Book>();
 
-        }
+            using (StreamReader sr = new StreamReader("Books.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string title = sr.ReadLine();
+                    string author = sr.ReadLine();
+                    string genre = sr.ReadLine();
+                    string isbn = sr.ReadLine();
+
+                    //  Book book1 = new Book(title, author, genre, isbn);
+                    books.Add(new Book(title, author, genre, isbn));
+                }
+                for (int i = 0; i < books.Count; i++)
+                {
+                    if (books[i].ISBN == ISBN)
+                    {
+                        books[i].Title = Title;
+                        books[i].Author = Author;
+                        books[i].Genre = Genre;
+                        books[i].ISBN = ISBN;
+                    }
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("Books.txt"))
+            {
+                for (int i = 0; i < books.Count; i++)
+                {
+                    sw.WriteLine("{0}\n{1}\n{2}\n{3}", books[i].Title, books[i].Author, books[i].Genre, books[i].ISBN);
+                }
+            }
+        
+    }
 
         /// <summary>
         ///  Reads in all book instances from file, then deletes the book with a certain ISBN number, then saves all book instances.
@@ -92,9 +197,38 @@ namespace BookReaderConsole
         /// <param name="ISBN"></param>
         static void Delete(string ISBN)
         {
+            List<Book> books = new List<Book>();
 
+            using (StreamReader sr = new StreamReader("Books.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string title = sr.ReadLine();
+                    string author = sr.ReadLine();
+                    string genre = sr.ReadLine();
+                    string isbn = sr.ReadLine();
+
+                    //  Book book1 = new Book(title, author, genre, isbn);
+                    books.Add(new Book(title, author, genre, isbn));
+                }
+                for (int i = 0; i < books.Count; i++)
+                {
+                    if (books[i].ISBN == ISBN)
+                    {
+                        books.Remove(books[i]);
+                        break;
+
+                    }
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("Books.txt"))
+            {
+                for (int i = 0; i < books.Count; i++)
+                {
+                    sw.WriteLine("{0}\n{1}\n{2}\n{3}", books[i].Title, books[i].Author, books[i].Genre, books[i].ISBN);
+                }
+            }
         }
-
     }
 
     class Book
