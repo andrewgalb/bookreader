b63f836e1a58d4a776a42179beb7cadaa29a838f
Someonexddd <50412783+Someonexddd@users.noreply.github.com>
2022-01-27 13:06:07 +0100

    sql added

---
+++ SQL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+using SQL;
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn;
+
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            conn = new SqlConnection(connString);
+
+
+            conn.Open();
+
+            Console.WriteLine("Conn open");
+
+            conn.Close();
+
+        }
+    }
+}
+    
